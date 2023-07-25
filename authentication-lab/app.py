7519fa320c2a60077fa3041c8815dcb3df3f0931
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2023-07-25 11:56:29 +0300

    done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -11,16 +11,11 @@ config = {
   "messagingSenderId": "200153313749",
   "appId": "1:200153313749:web:638242bcaa8eab4a818d5c",
   "measurementId": "G-9WRT1JPEG0",
-  "databaseURL":"https://console.firebase.google.com/project/ramiproject-1e8b8/database/ramiproject-1e8b8-default-rtdb/data/~2F"
-};
+  "databaseURL":"https://ramiproject-1e8b8-default-rtdb.firebaseio.com/"
+}
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-
-
-
-
-
-
+db = firebase.database()
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -37,32 +32,56 @@ def signin():
             x = request.form['password']
             auth.sign_in_with_email_and_password(z,x)
             return render_template("add_tweet.html")
-        except :
-            render_template("signin.html")
-        return "there was an erorr"
+        except Exception as e:
+            print(e)
+            return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-  user = {"email":request.form['email'],"password":request.form['password'],"fullname":request.form['fullname'] ,"username":request.form['username'],"bio":request.form['bio']}
-  uid = login_session['user']['localId']
+  
+  
   if request.method == 'GET':
     return render_template('signup.html')
   else:
     try:
+      user = {"email":request.form['email'],"fullname":request.form['fullname'] ,"username":request.form['username'],"bio":request.form['bio']}
+      print(user)
       z =request.form['email'] 
       x = request.form['password']
       login_session['user']= auth.create_user_with_email_and_password(z,x)
+      uid = login_session['user']['localId']
+      db.child("users").child(uid).set(user)
       return render_template('add_tweet.html')
-    except:
-        return render_template("add_tweet.html")
-    return "there was an erorr"
+    except Exception as e :
+        print(e)
+        return render_template("signup.html")
+    
 
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+    
+    if request.method == 'GET':
+        return redirect(url_for('add_tweet'))
+    else:
+        try:
+            tweet= {"title":request.form["title"],"text":request.form["text"],"uid":login_session['user']["localId"]}
+            db.child("tweets").push(tweet)
+            print(tweet)
+            return redirect(url_for('all_tweets'))
+        except:
+            return redirect('add_tweet.html')
+
+
+
+@app.route('/alltweets', methods=['GET', 'POST'])
+def all_tweets():
+   print("wdawdawdaw")
+   print(db.child("tweets").get().val())
+   return render_template('alltweets.html',data = db.child("tweets").get().val())
+
 
 
 if __name__ == '__main__':
